cac955ad95b1949b5d6401cc2d4592de34a87b2c
Tamim <1hk17is082@hkbk.edu.in>
2024-07-27 22:50:50 +0530

    adding two postive numbers of n digits

--- Main.java
+++ Main.java
@@ -3,13 +3,59 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        StringBuilder a = new StringBuilder();
+        StringBuilder b = new StringBuilder();
         Scanner sc = new Scanner(System.in);
         System.out.println("please enter a number");
-        String  a = sc.next() ;
+          a = new StringBuilder(sc.next());
         System.out.println("please enter b number");
-        String b = sc.next();
-        Integer sum = Integer.valueOf(a)+ Integer.valueOf(b);
-        System.out.print(sum);
+         b = new StringBuilder(sc.next());
+
+
+        int lengthA= a.length();
+        int lengthB= b.length();
+
+        if (lengthA != lengthB && lengthA > lengthB){
+            int difference= lengthA - lengthB;
+            int i=0;
+            b.reverse();
+            while( i<difference) {
+                b.append("0");
+                i++;
+            }
+            b.reverse();
+
+            addString(a.toString(),b.toString());
+        }
+        else if (lengthA != lengthB && lengthA < lengthB) {
+            int difference= lengthB -lengthA;
+            int i =0;
+            a.reverse();
+            while (i<difference){
+                a.append("0");
+                i++;
+            }
+            a.reverse();
+            addString(a.toString(),b.toString());
+        }
 
     }
+    public static void addString(String b, String c){
+        StringBuilder sumString = new StringBuilder();
+        int carry = 0;
+        for (int i = b.length() - 1; i >= 0; i--) {
+            int digitB = Integer.parseInt(String.valueOf(b.charAt(i)));
+            int digitC = Integer.parseInt(String.valueOf(c.charAt(i)));
+            int sumDigits = digitB + digitC + carry;
+            carry = sumDigits / 10;
+            sumString.append(sumDigits % 10);
+
+        }
+        if (carry != 0) {
+            sumString.append(carry);
+        }
+        String sumResult = sumString.reverse().toString();
+        System.out.println("Sum: " + sumResult);
+    }
+
 }
